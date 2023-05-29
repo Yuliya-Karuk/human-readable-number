12f7d5ecc6960e1e58d865aba98335fd00334d97
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-05-29 16:49:20 +0400

    feat: add solvetion for algoritmic task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function toReadable (number) {
-  
+    let arr = Array.from(String(number))
+    let result = ``
+   
+    if (arr.length === 3) {
+        result = result + message[arr[0]] + ' hundred '
+        arr.shift()
+       
+        if (arr[0] === '0' && arr[1] === '0') {
+            return result
+        }
+       
+        if (arr[0] === '0' && arr[1] !== '0') {
+            result = result + message[arr[1]]
+            return result
+        }
+       
+    }
+   
+    if ((arr[0] === '1' && arr.length === 2) || (arr.length === 2 && arr[1] === '0')) {
+        result = result + message[(arr[0]) + (arr[1])]
+        return result
+    }
+   
+    if (arr.length === 2) {
+        result = result + message[arr[0] + '0'] + ' '
+        arr.shift()
+    }
+   
+    if (arr.length === 1) {
+        result = result + message[arr[0]]
+        return result
+    }
 }
